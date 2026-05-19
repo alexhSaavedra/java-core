b0ac4bf971284e0eb09fe084e0544432b0629917
alex saavedra <alexhectorsaa30@gmail.com>
2026-05-19 14:27:44 -0400

    aded logical selection clases time

---
+++ core-elements/Switch.java
@@ -0,0 +1,21 @@
+public class Switch{
+    public static void main(String[] args) {
+        
+        int n = 7;
+
+        switch (n) {
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+              break;
+            case 1:
+                System.out.println("1");
+               break;    
+            default:
+                System.out.println("anything else");
+                break;
+        }
+    }
+}
---
+++ core-elements/Ternary.java
@@ -0,0 +1,12 @@
+public class Ternary {
+    public static void main(String[] args) {
+        
+        int number = 26;
+        int result = 0;
+
+        result= number>10? 10:20;
+
+        System.out.println(result);
+
+    }
+}
