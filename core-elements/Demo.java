f6ecd5f4a332de75628a7d558f146e4a12deceaf
alex saavedra <alexhectorsaa30@gmail.com>
2026-05-19 14:20:18 -0400

    added else statement to repository

---
+++ core-elements/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        int i = 5;
+
+        if(i>6){
+            System.out.println("i is greather than 6");
+        }else{
+            System.out.println("i is lower than 6");
+        }
+    }
+}
