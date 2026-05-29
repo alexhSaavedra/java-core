dffc4fd6600d1261ed83abed7cfb93d4d463170e
alex saavedra <alexhectorsaa30@gmail.com>
2026-05-29 07:50:16 -0400

    added jagged array

---
+++ clases/JaggedArray.java
@@ -0,0 +1,27 @@
+package clases;
+
+public class JaggedArray {
+    public static void main(String[] args) {
+        int array[][] = new int[3][];
+
+        array[0] = new int[3];
+        array[1] = new int[2];
+        array[2] = new int[1];
+
+        for(int i =0; i<array.length; i++){
+
+            for(int j=0; j<array[i].length;j++){
+                array[i][j] = (int)(Math.random()*10);
+            }
+        }
+
+        for(int n[]: array){
+
+            for(int m: n){
+                System.out.print(m + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
